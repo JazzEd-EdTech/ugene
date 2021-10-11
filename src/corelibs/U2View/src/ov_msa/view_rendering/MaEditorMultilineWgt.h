174658c86aab58ba5ccb58882ea6e60552533a16
ichebyki <ichebyki@gmail.com>
2021-10-11 20:22:45 +0700

    The init commit for UGENE-7042

--- src/corelibs/U2View/src/ov_msa/view_rendering/MaEditorMultilineWgt.h
+++ src/corelibs/U2View/src/ov_msa/view_rendering/MaEditorMultilineWgt.h
@@ -19,8 +19,8 @@
  * MA 02110-1301, USA.
  */
 
-#ifndef _U2_MA_EDITOR_WGT_H_
-#define _U2_MA_EDITOR_WGT_H_
+#ifndef _U2_MA_EDITOR_MULTILINE_WGT_H_
+#define _U2_MA_EDITOR_MULTILINE_WGT_H_
 
 #include <QWidget>
 
@@ -34,7 +34,7 @@ class QVBoxLayout;
 
 namespace U2 {
 
-class BaseWidthController;
+class MultilineBaseWidthController;
 class DrawHelper;
 class GScrollBar;
 class MaEditorConsensusArea;
@@ -42,55 +42,35 @@ class MSAEditorOffsetsViewController;
 class MaEditorStatusBar;
 class MaEditor;
 class MaEditorNameList;
-class MaEditorOverviewArea;
+class MaEditorMultilineOverviewArea;
 class MaEditorSequenceArea;
 class RowHeightController;
-class ScrollController;
+class MsaUndoRedoFramework;
+class MultilineScrollController;
 class SequenceAreaRenderer;
 
 /************************************************************************/
-/* MaEditorWgt */
+/* MaEditorMultilineWgt */
 /************************************************************************/
-class U2VIEW_EXPORT MaEditorWgt : public QWidget {
+class U2VIEW_EXPORT MaEditorMultilineWgt : public QWidget {
     Q_OBJECT
 public:
-    MaEditorWgt(MaEditor *editor);
-
-    QWidget *createHeaderLabelWidget(const QString &text = QString(),
-                                     Qt::Alignment ali = Qt::AlignCenter,
-                                     QWidget *heightTarget = nullptr,
-                                     bool proxyMouseEventsToNameList = true);
+    MaEditorMultilineWgt(MaEditor *editor);
 
     /** Returns MA editor instance. The instance is always defined and is never null. */
     MaEditor *getEditor() const;
 
-    MaEditorSequenceArea *getSequenceArea() const {
-        return sequenceArea;
-    }
-
-    MaEditorNameList *getEditorNameList() const {
-        return nameList;
-    }
-
-    MaEditorConsensusArea *getConsensusArea() const {
-        return consensusArea;
-    }
-
-    MaEditorOverviewArea *getOverviewArea() const {
+    MaEditorMultilineOverviewArea *getOverviewArea() const {
         return overviewArea;
-    }
-
-    MSAEditorOffsetsViewController *getOffsetsViewController() const {
-        return offsetsViewController;
     }
 
     MaEditorStatusBar *getStatusBar() const;
 
-    ScrollController *getScrollController() const {
+    MultilineScrollController *getScrollController() const {
         return scrollController;
     }
 
-    BaseWidthController *getBaseWidthController() const {
+    MultilineBaseWidthController *getBaseWidthController() const {
         return baseWidthController;
     }
 
@@ -98,17 +78,14 @@ public:
         return rowHeightController;
     }
 
-    DrawHelper *getDrawHelper() const {
-        return drawHelper;
-    }
-
     /* If 'true' and collapse group has only 1 row it will have expand/collapse control. */
     bool isCollapsingOfSingleRowGroupsEnabled() const {
         return enableCollapsingOfSingleRowGroups;
     }
 
-    QWidget *getHeaderWidget() const {
-        return seqAreaHeader;
+    int getSequenceAreaWidth() {
+        // Need to override in subclass
+        return 100;
     }
 
 signals:
@@ -116,48 +93,29 @@ signals:
     void si_stopMaChanging(bool modified = false);
     void si_completeRedraw();
 
+private slots:
+
 protected:
     virtual void initWidgets();
     virtual void initActions();
 
-    virtual void initSeqArea(GScrollBar *shBar, GScrollBar *cvBar) = 0;
-    virtual void initOverviewArea(MaEditorOverviewArea *overviewArea = nullptr) = 0;
-    virtual void initNameList(QScrollBar *nhBar) = 0;
-    virtual void initConsensusArea() = 0;
-    virtual void initStatusBar(MaEditorStatusBar *statusBar = nullptr) = 0;
+    virtual void initOverviewArea(MaEditorMultilineOverviewArea *overviewArea = nullptr) = 0;
+    virtual void initStatusBar() = 0;
 
 protected:
     MaEditor *const editor;
-    MaEditorSequenceArea *sequenceArea;
-    MaEditorNameList *nameList;
-    MaEditorConsensusArea *consensusArea;
-    MaEditorOverviewArea *overviewArea;
+    MaEditorMultilineOverviewArea *overviewArea;
     MaEditorStatusBar *statusBar;
-    MSAEditorOffsetsViewController *offsetsViewController;
-
-    QWidget *nameAreaContainer;
-    QWidget *seqAreaHeader;
-    QVBoxLayout *seqAreaHeaderLayout;
-
-    QGridLayout *seqAreaLayout;
-    QVBoxLayout *nameAreaLayout;
-    MaSplitterController maSplitter;
 
     bool enableCollapsingOfSingleRowGroups;
-    ScrollController *scrollController;
-    BaseWidthController *baseWidthController;
+    MultilineScrollController *scrollController;
+    MultilineBaseWidthController *baseWidthController;
     RowHeightController *rowHeightController;
-    DrawHelper *drawHelper;
 
 public:
-    QAction *delSelectionAction;
-    QAction *copySelectionAction;
-    QAction *copyFormattedSelectionAction;
-    QAction *pasteAction;
-    QAction *pasteBeforeAction;
-    QAction *cutSelectionAction;
+
 };
 
 }  // namespace U2
 
-#endif  // _U2_MA_EDITOR_WGT_H_
+#endif  // _U2_MA_EDITOR_MULTILINE_WGT_H_
